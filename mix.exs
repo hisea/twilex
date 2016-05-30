c8ad3d79a0884afcf7f2f235fcac95d5eb362b22
Yinghai Zhao <zyinghai@gmail.com>
2016-05-30 10:55:45 -0400

    Bump version to 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Twilex.Mixfile do
 
   def project do
     [app: :twilex,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      description: "A Twilio client for elixir.",
      build_embedded: Mix.env == :prod,
